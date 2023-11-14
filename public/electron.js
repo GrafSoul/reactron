21c68ef08a83492115a75cc5cd2f4c79eb73c35a
GrafSoul <grafsoul@gmail.com>
2023-11-14 09:18:20 +0500

    feat: Updated Reactron

--- public/electron.js
+++ public/electron.js
@@ -1,16 +1,22 @@
 'use strict';
 
-const { app, BrowserWindow } = require("electron");
-const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
-const path = require("path");
-const isDev = require("electron-is-dev");
+const { app, BrowserWindow } = require('electron');
+const {
+    default: installExtension,
+    REACT_DEVELOPER_TOOLS,
+    REDUX_DEVTOOLS,
+} = require('electron-devtools-installer');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 const getIcon = () => {
-    if (process.platform === 'win32') return `${path.join(__dirname, "/icons/icon.ico")}`;
-    if (process.platform === 'darwin') return `${path.join(__dirname, "/icons/icon.icns")}`;
-    return `${path.join(__dirname, "/icons/16x16.png")}`;
+    if (process.platform === 'win32')
+        return `${path.join(__dirname, '/icons/icon.ico')}`;
+    if (process.platform === 'darwin')
+        return `${path.join(__dirname, '/icons/icon.icns')}`;
+    return `${path.join(__dirname, '/icons/16x16.png')}`;
 };
 
 let mainWindow;
@@ -22,16 +28,17 @@ const createWindow = async () => {
         icon: getIcon(),
         width: 800,
         height: 600,
+        applicationSupportsSecureRestorableState: true,
         webPreferences: {
             nodeIntegration: true,
-            webSecurity: false
-        }
+            webSecurity: false,
+        },
     });
 
     mainWindow.loadURL(
         isDev
-            ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "../build/index.html")}`
+            ? 'http://localhost:3000'
+            : `file://${path.join(__dirname, '../build/index.html')}`,
     );
 
     mainWindow.once('ready-to-show', () => {
@@ -42,18 +49,18 @@ const createWindow = async () => {
         }
     });
 
-    mainWindow.on("closed", () => (mainWindow = null));
+    mainWindow.on('closed', () => (mainWindow = null));
 };
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
     if (mainWindow === null) {
         createWindow();
     }
